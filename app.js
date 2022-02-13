b04d01ff1068d3614e5b4ff74ee73b557650a655
mollikacomputer <mollikacomputer3@gmail.com>
2022-02-13 20:42:03 +0600

    cart value increase decrease

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('I am in a separate file')
+function upgradeCaseNumber(isAdd){
+    const caseInput = document.getElementById('case-input');
+    let caseNumber = caseInput.value;
+    if(isAdd == true){
+        caseNumber = parseInt(caseNumber)+1;
+    }else{
+        caseNumber = parseInt(caseNumber)-1;
+    }
+    caseInput.value = caseNumber;
+    // update case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+}
+document.getElementById('case-plus').addEventListener('click', function(){
+    upgradeCaseNumber(true);
+    
+})
+document.getElementById('case-minus').addEventListener('click', function(){
+    upgradeCaseNumber(false);
+})
